8256842e372488954c88b9a98dae6cfbcd3e26e5
Carlos Campoy <36555654+lospoy@users.noreply.github.com>
2024-05-06 12:58:07 -0400

    style and copy: links, feed, post-event-markdown

--- src/markdown/post-event-markdown.ts
+++ src/markdown/post-event-markdown.ts
@@ -2,11 +2,10 @@ import type {HydratedFeedItemPreHtml} from '../types'
 
 export default ({event, permalink}: HydratedFeedItemPreHtml) => `
 # [Slides: ${event.data.title}](${permalink})
-
 ${event.data.banner && `<img src="${event.data.banner}" style="width:500px"/>`}
 <br>
 Special thanks to our presenters:
 <br><br>
 
-${event.presentations.map(p => `* ${p.speaker.data.name} - ${p.data.title} - [slides](${p.data.slides})`).join('\n')}
+${event.presentations.map(p => `* ${p.speaker.data.name} <br> ${p.data.title} - [slides](${p.data.slides})<br><br>`).join('\n')}
 `
